2d136cb02459945290f26e64b1ebfe35fb0e175f
jaugros <jaugros@ctepl.com>
2012-02-27 18:04:02 +0000

    Committing...

---
+++ cookbooks/asdf/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "CTE"
+maintainer_email "jaugros@ctepl.com"
+license          "All rights reserved"
+description      "Installs/Configures asdf"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+recipe "asdf::default", "Prints a message to the output"
---
+++ cookbooks/asdf/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: asdf
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hi."
